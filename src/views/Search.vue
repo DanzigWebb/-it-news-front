1e13f2094cf2864741bd8a0a978474704862bf13
Alexandr <valverin95@gmail.com>
2020-11-06 21:33:13 +0300

    refactor & create LoaderState

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,18 +5,25 @@
     <div class="pa-md-10 pa-3" v-if="posts.length">
       <p>Кол-во статей просканировано: <b>{{ quantity }}</b></p>
       <p>Найдено: <b>{{ posts.length }}</b></p>
+
       <PostItem
-          v-for="post in posts"
+          v-for="post in visiblePosts"
           :post="post"
           :key="post.title"
       />
+
+      <v-btn v-if="posts.length > visiblePosts.length"
+             @click="setVisiblePosts()"
+             color="primary">
+        <span>Больше постов ({{this.visiblePosts.length }} / {{ this.posts.length }})</span>
+      </v-btn>
     </div>
 
   </v-container>
 </template>
 
 <script>
-import PostsForm from '@/components/PostsForm'
+import PostsForm from '@/components/posts/PostsForm'
 import PostItem from '@/components/posts/PostItem'
 import getPosts from '@/services/posts/getPosts'
 
@@ -27,6 +34,10 @@ export default {
     PostsForm
   },
   data: () => ({
+    visiblePosts: [],
+    step: 5,
+    currentStep: 0,
+
     posts: [],
     quantity: 0,
     isLoad: false
@@ -37,7 +48,13 @@ export default {
       const {posts, quantity} = response
       this.posts = posts
       this.quantity = quantity
-    }
+      this.setVisiblePosts()
+    },
+
+    setVisiblePosts() {
+      this.currentStep = this.currentStep + this.step
+      this.visiblePosts = this.posts.slice(0, this.currentStep)
+    },
   }
 }
 </script>
